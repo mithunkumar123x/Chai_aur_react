72f71d7a67156bf24877f2c56502e75cc87771bf
mithunkumar123x <mithunkumar123x@gmail.com>
2024-02-20 14:24:11 +0530

    add some condition#

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,24 +1,38 @@
-import { useState } from 'react'
+import  { useState  } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-   
+
+  let  [counter,changeCounter] =  useState(15)
+    
+   // let counter = 15
   const addValue = () => {
-    console.log("clicked", counter)   
-      counter = counter + 1 
+   if(counter < 20) {
+      //counter = counter + 1 ;
+      changeCounter(counter + 1 )
+      console.log("clicked", counter)  
+     }
   }
-  let counter = 15
+  const removeValue = () => {
+   if(counter > 0) {
+    counter = counter - 1 ;
+     changeCounter(counter)
+     console.log("clicked", counter)
+   }
+  }
+
   return (
     <>
     <h1>Chai aur react</h1>
     <h2>Counter value: {counter}</h2>
-
     <button
-    onClick={addValue}>Add value</button>
+    onClick={addValue}>Add value {counter}</button>
     <br />
-    <button>remove value</button>
+    < button 
+    onClick={removeValue}>remove value {counter} </button>
+     <p> footer: {counter} </p>
     </> 
   )
 }
